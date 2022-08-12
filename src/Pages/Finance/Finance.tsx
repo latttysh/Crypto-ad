6b1681ea01a3d1f0aae0bfc31da68fd7b9aaaacd
latttysh <semyon.latysh@gmail.com>
2022-08-12 18:09:17 +0300

    Страница финансов (без иконок и красок)

--- src/Pages/Finance/Finance.tsx
+++ src/Pages/Finance/Finance.tsx
@@ -16,6 +16,11 @@ const Finance: FunctionComponent<Props> = (props) => {
                       <label>Сумма пополнения</label>
                       <input type="text" placeholder={"0,000$"}/>
                   </div>
+                  <div className={s.choose}>
+                      <div className={s.choose__title}>Способ оплаты</div>
+                      <div className={s.choose__block}>Bitcoin (BTC)</div>
+                  </div>
+                  <button className={s.button}>Пополнить</button>
               </div>
               <div className={s.withdraw}>
                   <div className={s.title}>Вывод средств</div>
@@ -23,7 +28,48 @@ const Finance: FunctionComponent<Props> = (props) => {
                       <label>Сумма Вывода</label>
                       <input type="text" placeholder={"0,000$"}/>
                   </div>
+                  <div className={s.choose}>
+                      <div className={s.choose__title}>Валюта вывода</div>
+                      <div className={s.choose__block}>Bitcoin (BTC)</div>
+                  </div>
+                  <button className={s.button}>Вывести средства</button>
               </div>
+          </div>
+          <div className={s.history}>
+              <div className={s.title}>История транзакций</div>
+              <table>
+                  <thead>
+                  <tr>
+                      <th>Сумма</th>
+                      <th>Тип транзакции</th>
+                      <th>Дата</th>
+                      <th>Статус</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  <tr>
+                      <td>1 000,000$</td>
+                      <td>Вывод</td>
+                      <td>10:13 <br/>
+                          09/08/2022</td>
+                      <td>В процессе</td>
+                  </tr>
+                  <tr>
+                      <td>2 000,000$</td>
+                      <td>Вывод</td>
+                      <td>10:13 <br/>
+                          09/08/2022</td>
+                      <td>Ошибка</td>
+                  </tr>
+                  <tr>
+                      <td>2 000,000$</td>
+                      <td>Вывод</td>
+                      <td>10:13 <br/>
+                          09/08/2022</td>
+                      <td>Ошибка</td>
+                  </tr>
+                  </tbody>
+              </table>
           </div>
       </div>
   );
